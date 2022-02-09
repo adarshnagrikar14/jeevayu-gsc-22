9b28901c6fa02e25808e8c9bd272ee88cfcbb394
Adarsh <adarshnagrikar1404@gmail.com>
2022-02-09 17:38:26 +0530

    small updates

--- android/app/src/main/java/com/example/jeevayu/MainActivity.java
+++ android/app/src/main/java/com/example/jeevayu/MainActivity.java
@@ -1,6 +1,94 @@
 package com.example.jeevayu;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Method;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+
+    static final String CHANNEL = "com.project/battery";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+        BinaryMessenger messenger = flutterEngine.getDartExecutor().getBinaryMessenger();
+        MethodChannel channel = new MethodChannel(messenger, CHANNEL);
+
+        channel.setMethodCallHandler(((call, result) -> {
+            if (call.method.equals("getBatteryLevel")) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!Settings.System.canWrite(getApplicationContext())) {
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName()));
+                        startActivityForResult(intent, 200);
+
+                    } else {
+                        // setPass(getContext());
+                        String pass = getPass(getContext());
+                        result.success(pass);
+                    }
+                }
+
+            } else {
+                result.notImplemented();
+            }
+        }));
+
+    }
+
+    private void setPass(Context context) {
+        try {
+
+            assert context != null;
+            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            Method getConfigMethod = wifiManager.getClass().getMethod("getWifiApConfiguration");
+            WifiConfiguration wifiConfiguration = (WifiConfiguration) getConfigMethod.invoke(wifiManager);
+            assert wifiConfiguration != null;
+            wifiConfiguration.SSID = "Name";
+            wifiConfiguration.preSharedKey = "Password";
+
+            Method setConfigMethod = wifiManager.getClass().getMethod("setWifiApConfiguration", WifiConfiguration.class);
+            setConfigMethod.invoke(wifiManager, wifiConfiguration);
+
+        } catch (Exception e) {
+            Log.e("TAG", "onCreate: ", e);
+        }
+    }
+
+    private String getPass(Context context) {
+
+        String level;
+        try {
+
+            assert context != null;
+            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            Method getConfigMethod = wifiManager.getClass().getMethod("getWifiApConfiguration");
+            WifiConfiguration wifiConfiguration = (WifiConfiguration) getConfigMethod.invoke(wifiManager);
+            assert wifiConfiguration != null;
+
+            // level = wifiConfiguration.SSID;
+            level = wifiConfiguration.preSharedKey;
+            return level;
+
+        } catch (Exception e) {
+            Log.e("TAG", "onCreate: ", e);
+            return null;
+
+        }
+
+    }
 }
--- lib/classes/home.dart
+++ lib/classes/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({Key? key}) : super(key: key);
@@ -8,12 +9,35 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  static const batteryChannel = MethodChannel('com.project/battery');
+
+  String _password = 'Getting..';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Text(_password),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              getBatteryLevel();
+            },
+            child: const Text('Get Password'),
+          ),
+        ],
       ),
     );
+  }
+
+  Future getBatteryLevel() async {
+    final String _newPass =
+        await batteryChannel.invokeMethod('getBatteryLevel');
+    setState(() {
+      _password = _newPass;
+    });
   }
 }
